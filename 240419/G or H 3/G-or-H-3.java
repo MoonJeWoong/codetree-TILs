25739965a843a9884afebbbba6e5e1ff3e55f02f
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-19 23:00:34 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240419/G or H 3/G-or-H-3.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int n = inputs[0], k = inputs[1];
+        int[] locations = new int[10_001];
+
+        for (int iter = 0; iter < k; iter++) {
+            String[] inputLine = br.readLine().split(" ");
+            int idx = Integer.parseInt(inputLine[0]);
+            if (inputLine[1].equals("G")) {
+                locations[idx] = 1;
+            } else {
+                locations[idx] = 2;
+            }
+        }
+
+        int lt = 1,  rt = lt + k + 1;
+        int answer = 0;
+        for (int idx = lt; idx < rt; idx++) {
+            answer += locations[idx];
+        }
+
+        int tmpSum = answer;
+        for (int idx = rt; idx < locations.length - k; idx++) {
+            tmpSum = tmpSum - locations[lt] + locations[rt];
+            answer = Math.max(tmpSum, answer);
+        }
+        System.out.println(answer);
+    }
+}
