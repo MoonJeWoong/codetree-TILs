c89e5f67a5421fea95e0214cc89298f2b0c341ab
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-22 21:26:52 +0900

    Title: 이상한 폭탄, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240422/이상한 폭탄/strange-bomb.py
+++ 240422/이상한 폭탄/strange-bomb.py
@@ -2,15 +2,14 @@ N, K = list(map(int,input().split()))
 bomb_nums = list()
 for i in range(N):
     num = int(input())
-    bomb_nums.append((num,i))
-bomb_cnt = dict()
+    bomb_nums.append(num)
+last_idx = dict()
 
 max_val = -1
-for num, i in bomb_nums:
-    if num in bomb_cnt and bomb_cnt[num] > 0 :
-        max_val = max(num, max_val)
-    for n in bomb_cnt.keys():
-        bomb_cnt[n] -= 1
-    bomb_cnt[num] = K
-    # print(i, num, bomb_cnt)
+for i, num in enumerate(bomb_nums):
+    if num in last_idx:
+        if i - last_idx[num] < K:
+            max_val = max(num, max_val)
+    last_idx[num] = i
+
 print(max_val)
