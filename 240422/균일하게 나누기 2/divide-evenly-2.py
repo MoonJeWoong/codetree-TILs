0d507a68b05a1d8e628f70b6ef3f8a6c2aa74ab7
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-22 21:56:28 +0900

    Title: 균일하게 나누기 2, Time: 261ms, Memory: 26MB, Status: Passed - CodeTree

--- 240422/균일하게 나누기 2/divide-evenly-2.py
+++ 240422/균일하게 나누기 2/divide-evenly-2.py
@@ -16,14 +16,17 @@ for b in range(0,1001,2):
             cnt[0] += 1
         else:
             cnt[3] += 1 
-    
-    for x, y in points:
-        min_val = min(min_val,max(cnt))
+    # min_val = min(min_val,max(cnt))
+    for i, (x, y) in enumerate(points):
+        if i == 0 or x != points[i-1][0]:
+            min_val = min(min_val,max(cnt))
         if y > b:
             cnt[0] -= 1
             cnt[1] += 1
         else:
             cnt[3] -=1
             cnt[2] += 1
-    min_val = min(min_val,max(cnt))
+        
+ 
+    # min_val = min(min_val,max(cnt))
 print(min_val)
