81d4fc0490acd2cccf54c9a1623963d772c9addc
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-22 20:15:12 +0900

    Title: 최고의 33위치, Time: 241ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240422/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] maps = new int[n][n];
+        for (int iter = 0; iter < n; iter++) {
+            maps[iter] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        // for 문 순회하면서 전체 탐색
+        // 첫 for문으로 왼쪽 상단 가장자리 좌표를 선택
+        // 이후 이중 for문으로 3 by 3 좌표들을 탐색하며 카운트
+        // 최대 동전의 수 갱신
+
+        int answer = Integer.MIN_VALUE;
+        int tmpSum;
+        for (int row = 0; row <= maps.length - n; row++) {
+            for (int col = 0; col <= maps.length - n; col++) {
+                tmpSum = 0;
+                for (int windowRow = row; windowRow < row + n; windowRow++) {
+                    for (int windowCol = col; windowCol < col + n; windowCol++) {
+                        if (maps[windowRow][windowCol] == 1) {
+                            tmpSum++;
+                        }
+                    }
+                }
+                answer = Math.max(answer, tmpSum);
+            }
+        }
+        System.out.println(answer);
+    }
+}
