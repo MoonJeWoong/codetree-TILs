2522967919796df06f45f03a2b2501e256cff096
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-22 21:24:31 +0900

    Title: 이상한 폭탄, Time: 5083ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240422/이상한 폭탄/strange-bomb.py
+++ 240422/이상한 폭탄/strange-bomb.py
@@ -7,7 +7,7 @@ bomb_cnt = dict()
 
 max_val = -1
 for num, i in bomb_nums:
-    if num in bomb_cnt and bomb_cnt[num] >= 0 :
+    if num in bomb_cnt and bomb_cnt[num] > 0 :
         max_val = max(num, max_val)
     for n in bomb_cnt.keys():
         bomb_cnt[n] -= 1
