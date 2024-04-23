9440955695d0d6b36e6305b57088913aa18b639c
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-04-23 21:42:58 +0900

    Title: 겹치지 않게 선분 고르기, Time: 176ms, Memory: 25MB, Status: Passed - CodeTree

--- 240423/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240423/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -12,13 +12,16 @@ def is_overlap(x1,x2, lines):
     for a,b in lines:
         if a<=x1<=b or a<=x2<=b:
             return True
+        if x1<=a<=x2 or x1<=b<=x2:
+            return True
 
     return False
 
 def backtrack(cnt, selected):
     global max_val
     if cnt == N:
-
+        # if len(selected)==3:
+        #     print(selected)
         max_val = max(len(selected),max_val)
         return
 
