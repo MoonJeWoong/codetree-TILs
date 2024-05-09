d48c46f5a3aec868bb4ae71564e0a759be52b159
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-09 22:38:44 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 272ms, Memory: 28MB, Status: Passed - CodeTree

--- 240509/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
+++ 240509/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -3,7 +3,6 @@ dist = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
 for i in range(M):
     v1, v2, d = list(map(int,input().split()))
     dist[v1][v2] = min(d,dist[v1][v2])
-    # dist[v2][v1] = min(d,dist[v2][v1])
 
 for i in range(1,N+1):
     dist[i][i] = 0
@@ -11,12 +10,11 @@ for i in range(1,N+1):
 for k in range(1,N+1):
     for i in range(1,N+1):
         for j in range(1,N+1):
-            dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
-
+            dist[i][j] = min(dist[i][j], dist[i][k]+ dist[k][j])
 
 for i in range(1,N+1):
     for j in range(1,N+1):
-        v = dist[i][j] 
-        if dist[i][j] == float('inf'): v = -1
+        v = dist[i][j]
+        if v == float('inf'): v=-1
         print(v,end=' ')
     print()
