c707475436f1db56701e943d65b6c80fbe08af81
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-09 21:15:53 +0900

    Title: 각 정점까지의 최단 경로, Time: 823ms, Memory: 49MB, Status: Passed - CodeTree

--- 240509/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 240509/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -12,25 +12,27 @@ for _ in range(M):
     graph[v1].append((w,v2))
     graph[v2].append((w,v1))
 
+visited = [False for _ in range(N+1)]
 
 dist[k] = 0
 
 import heapq as hq
 
-hq.heappush(pq,(0,k))
+hq.heappush(pq, (0,k))
 
 while pq:
-    min_dist, min_idx = hq.heappop(pq)
+    cost, idx = hq.heappop(pq)
 
-    if min_dist != dist[min_idx]: continue
+    if visited[idx]: continue
+    visited[idx] = True
 
-    for next_dist, next_idx in graph[min_idx]:
-        new_dist = dist[min_idx] + next_dist
-        if dist[next_idx] > new_dist:
-            dist[next_idx] = new_dist
-            hq.heappush(pq, (new_dist, next_idx))
-
-for i in range(1, N+1):
+    for near_cost, near_idx in graph[idx]:
+        new_dist = dist[idx] + near_cost
+        if dist[near_idx] > new_dist:
+            dist[near_idx] = new_dist
+            hq.heappush(pq, (new_dist, near_idx))
+for i in range(1, N + 1):
+    # 만약 도달이 불가능하다면 -1을 출력합니다.
     if dist[i] == INT_MAX:
         print(-1)
     else:
