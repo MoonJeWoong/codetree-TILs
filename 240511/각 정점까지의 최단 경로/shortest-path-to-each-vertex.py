f6ec2a6922cc45d8c8c0e036fdac5b3014e047d0
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-11 22:03:32 +0900

    Title: 각 정점까지의 최단 경로, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240511/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from heapq import heappop as pop, heappush as push
+N, M = list(map(int,input().split()))
+K = int(input())
+graph = defaultdict(list)
+for _ in range(N):
+    v1,v2,w = list(map(int,input().split()))
+    graph[v1].append((w,v2))
+    graph[v2].append((w,v1))
+
+
+dist = [float('inf') for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+dist[K] = 0
+
+que = [(0,K)]
+while que:
+    w, n = pop(que)
+    if visited[n]: continue
+    visited[n] = True
+
+    for near in graph[n]:
+        n_weight, n_node = near
+        if visited[n_node]: continue
+        new_weight = n_weight + w
+        if dist[n_node] > new_weight:
+            dist[n_node] = new_weight
+            push(que, (new_weight, n_node))
+
+for i in range(1,N+1):
+    d = dist[i] if dist[i] != float('inf') else -1
+    print(d)
