b501cf09efed09e45af5fefb2e076ac56a140848
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-11 18:13:38 +0900

    Title: 숫자의 개수, Time: 478ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240511/숫자의 개수/number-of-integers.py
@@ -0,0 +1,27 @@
+N, M = list(map(int,input().split()))
+nums = list(map(int,input().split()))
+def get_cnt(target):
+    left = 0
+    right = len(nums) - 1
+    lower_bound = len(nums)
+    while left <= right:
+        mid = (left+right) // 2
+        if nums[mid] >= target:
+            lower_bound = min(lower_bound, mid)
+            right = mid -1
+        else:
+            left = mid + 1
+    left = 0
+    right = len(nums) - 1
+    upper_bound = len(nums)
+    while left <= right:
+        mid = (left+right) // 2
+        if nums[mid] > target:
+            upper_bound = min(upper_bound, mid)
+            right = mid -1
+        else:
+            left = mid + 1
+    return upper_bound-lower_bound
+
+for i in range(M):
+    print(get_cnt(int(input())))
