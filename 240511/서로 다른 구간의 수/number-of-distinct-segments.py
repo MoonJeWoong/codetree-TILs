a4083794f851abcbbc441344d7b030817be3aa30
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-11 17:24:05 +0900

    Title: 서로 다른 구간의 수, Time: 998ms, Memory: 44MB, Status: Passed - CodeTree

--- 240511/서로 다른 구간의 수/number-of-distinct-segments.py
+++ 240511/서로 다른 구간의 수/number-of-distinct-segments.py
@@ -2,21 +2,22 @@ N = int(input())
 low_bound = float('inf')
 up_bound = 0
 lines = list()
-
+points = list()
 for i in range(N):
     a,b = list(map(int,input().split()))
-    low_bound = min(a, low_bound)
-    up_bound = max(b,up_bound)
-    lines.append((a,b))
+    points.append((a,1,i))
+    points.append((b,-1,i))
 
-overlap_arr = [0 for _ in range(up_bound+1)]
+points.sort()
 
-for a,b in lines:
-    overlap_arr[a] += 1
-    overlap_arr[b] -= 1
-num_line = 1
-for i in range(low_bound, up_bound):
-    overlap_arr[i] = overlap_arr[i] + overlap_arr[i-1]
-    if overlap_arr[i] == 0 and overlap_arr[i-1] != 0:
-        num_line += 1
-print(num_line)
+cnt = 0
+curr_point = set()
+for point in points:
+    num, flag, idx = point
+    if flag == -1:
+        curr_point.remove(idx)
+    elif flag == 1:
+        if not curr_point: cnt += 1
+        curr_point.add(idx)
+
+print(cnt)
