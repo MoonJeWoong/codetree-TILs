0a57aca40499b128234825c40215d795cdd81072
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-05-11 19:26:01 +0900

    Title: bit 계산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240511/bit 계산/bit-calculation.py
@@ -0,0 +1,34 @@
+Q = int(input())
+
+class Command():
+
+    def add(s,x):
+        if (s >> x) & 1 == 0:
+            s ^= (1<<x)
+        return s
+    def delete(s,x):
+        if (s >> x) & 1 == 1:
+            s ^= (1<<x)
+        return s
+    def print_(s,x):
+        print((s >> x) & 1)
+        return s        
+
+    def toggle(s,x):
+        s ^= (1<<x)
+        return s
+    def clear(s, x):
+        return 0
+
+
+s = 0
+for _ in range(Q):
+    order = input()
+    if order == 'clear':
+        val = None
+    else:
+        command, val = order.split()
+        val = int(val)
+        if command == 'print':
+            command += '_'
+    s = getattr(Command, command)(s,val)
