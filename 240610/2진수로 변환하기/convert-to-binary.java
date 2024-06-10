b524d738607d6e3fbcb82d87dcb169d830006e3d
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-06-10 10:05:05 +0900

    Title: 2진수로 변환하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240610/2진수로 변환하기/convert-to-binary.java
+++ 240610/2진수로 변환하기/convert-to-binary.java
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
         
         List nums = new ArrayList<>();
         while(true) {
