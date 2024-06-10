9b35da229f1118560601d1942a5541cd9d5e0875
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2024-06-10 13:01:18 +0900

    Title: 색종이의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240610/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] plane = new int[201][201];
+
+        for (int i=0; i<n; i++) {
+            int[] inputPoint = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            checkSquare(plane, inputPoint);
+        }
+
+        int sum = 0;
+        for (int row=0; row<plane.length; row++) {
+            for (int col=0; col<plane[0].length; col++) {
+                sum += plane[row][col];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void checkSquare(int[][] plane, int[] inputPoint) {
+        int offset = 100;
+        
+        int x1 = inputPoint[0];
+        int y1 = inputPoint[1];
+        int x2 = x1 + 8;
+        int y2 = y1 + 8;
+
+        for (int row=x1; row<x2; row++) {
+            for (int col=y1; col<y2; col++) {
+                plane[row][col] = 1;
+            }
+        }
+    }
+}
